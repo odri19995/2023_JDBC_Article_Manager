57d135685837e588a7610ba72dac0468a75fd23c
odri19995 <odri19995@gmail.com>
2023-03-08 13:42:28 +0900

    JDBC Insert Test

--- JDBC_Article_Manager/src/com/KoreaIT/example/JAM/Main.java
+++ JDBC_Article_Manager/src/com/KoreaIT/example/JAM/Main.java
@@ -9,36 +9,47 @@ public class Main {
 		System.out.println("==프로그램 시작==");
 
 		Scanner sc = new Scanner(System.in);
-		
+
 		List<Article> articles = new ArrayList<>();
 
 		int id = 0;
 		while (true) {
 			System.out.printf("명령어 ) ");
 			String cmd = sc.nextLine().trim();
-			if (cmd.equals("article list")) {
+			if (cmd.equals("article write")) {
 				id++;
 				System.out.print("제목 :  ");
 				String title = sc.nextLine();
 				System.out.print("내용 : ");
 				String body = sc.nextLine();
-				Article article = new Article(id,title, body);
+				Article article = new Article(id, title, body);
 				articles.add(article);
 				
-				
-				
-				System.out.printf("%d | %s\n",article.id, article.title);
-			} else if (cmd.equals("exit")) {
+				System.out.printf("%d 번 게시글이 생성되었습니다.\n", article.id);
+
+			} else if (cmd.equals("article list")) {
+				System.out.println("== 게시물 목록 ==");
+
+				if (articles.size() == 0) {
+					System.out.println("게시물이 없습니다");
+					continue;
+				}
+
+				System.out.println("번호	|	제목");
+
+				for (int i = 0 ; i < articles.size();i++){
+					Article article = articles.get(i);
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
+			}
+
+			if (cmd.equals("exit")) {
+				System.out.println("== 프로그램 종료 ==");
 				break;
-			} else {
-				System.out.println("존재하지 않는 명령어 입니다.");
 			}
 
 		}
-
-		System.out.println("==프로그램 종료==");
-
 		sc.close();
-	}
 
+	}
 }
