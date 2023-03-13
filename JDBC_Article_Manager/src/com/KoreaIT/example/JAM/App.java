6de353e50c91786509b34a1bb6f60917af427cc1
odri19995 <odri19995@gmail.com>
2023-03-13 14:50:37 +0900

    회원가입 구현

--- JDBC_Article_Manager/src/com/KoreaIT/example/JAM/App.java
+++ JDBC_Article_Manager/src/com/KoreaIT/example/JAM/App.java
@@ -28,7 +28,79 @@ public class App {
 				System.out.printf("명령어) ");
 				String cmd = sc.nextLine().trim();
 
-				if (cmd.equals("article write")) {
+				if (cmd.equals("member join")) {
+					String loginId = null;
+					String loginPw = null;
+					String loginPwChk = null;
+					String name = null;
+					System.out.println("== 회원 가입 ==");
+
+					while (true) {
+						System.out.printf("로그인 아이디 : ");
+						loginId = sc.nextLine().trim();
+
+						if (loginId.length() == 0) {
+							System.out.println("아이디를 입력해주세요");
+							continue;
+						}
+						break;
+					}
+
+					while (true) {
+						System.out.printf("로그인 비밀번호 : ");
+						loginPw = sc.nextLine().trim();
+
+						if (loginPw.length() == 0) {
+							System.out.println("비밀번호를 입력해주세요");
+							continue;
+						}
+
+						boolean loginPwCheck = true;
+
+						while (true) {
+							System.out.printf("로그인 비밀번호 확인 : ");
+							loginPwChk = sc.nextLine().trim();
+
+							if (loginPwChk.length() == 0) {
+								System.out.println("비밀번호 확인을 입력해주세요");
+								continue;
+							}
+
+							if (loginPw.equals(loginPwChk) == false) {
+								System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요");
+								loginPwCheck = false;
+							}
+							break;
+						}
+						if (loginPwCheck) {
+							break;
+						}
+					}
+					while (true) {
+						System.out.printf("이름 : ");
+						name = sc.nextLine().trim();
+
+						if (name.length() == 0) {
+							System.out.println("이름을 입력해주세요");
+							continue;
+						}
+						break;
+					}
+
+					SecSql sql = new SecSql();
+
+					sql.append("INSERT INTO `member`");
+					sql.append("SET regDate = NOW()");
+					sql.append(", updateDate = NOW()");
+					sql.append(", loginId = ?", loginId);
+					sql.append(", loginPw = ?", loginPw);
+					sql.append(", `name` = ?", name);
+
+					DBUtil.insert(conn, sql);
+
+					System.out.printf("%s 회원님 환영합니다\n", name);
+
+				} else if (cmd.equals("article write")) {
 					System.out.println("== 게시물 작성 ==");
 
 					System.out.printf("제목 : ");
